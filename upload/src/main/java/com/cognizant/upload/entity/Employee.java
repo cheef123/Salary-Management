18a127c09195e234a0fac86b5a08b0517dee03a2
Chee Fong <cheefong.ngeow@gmail.com>
2022-06-08 17:25:37 +0800

    Added column names to entity

--- upload/src/main/java/com/cognizant/upload/entity/Employee.java
+++ upload/src/main/java/com/cognizant/upload/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.cognizant.upload.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,14 +15,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
-	
+
 	@Id
+	@Column(name = "id")
 	private int empId;
-	
+
+	@Column(name = "login")
 	private String empLogin;
-	
+
+	@Column(name = "name")
 	private String empName;
-	
+
+	@Column(name = "salary")
 	private double empSalary;
 
 }
