e37f53d6724a50eccae6a9cff6143ace584aa370
Chee Fong <cheefong.ngeow@gmail.com>
2022-06-09 21:06:10 +0800

    Added new method to findbyemplogin

--- upload/src/main/java/com/cognizant/upload/repository/EmployeeRepository.java
+++ upload/src/main/java/com/cognizant/upload/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.cognizant.upload.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.cognizant.upload.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-
+	List<Employee> findByEmpLogin(String login);
+ 
 }
