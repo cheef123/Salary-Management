48808d43e244259b2471e0cf115fd78c7e8a65ee
Chee Fong <cheefong.ngeow@gmail.com>
2022-06-09 11:19:58 +0800

    Updated fields of customerrorresponse

--- upload/src/main/java/com/cognizant/upload/pojo/CustomErrorResponse.java
+++ upload/src/main/java/com/cognizant/upload/pojo/CustomErrorResponse.java
@@ -3,6 +3,8 @@ package com.cognizant.upload.pojo;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,15 @@ public class CustomErrorResponse {
 	 * This field will show the time when the exception occurs
 	 */
 	private LocalDateTime timestamp; 
+	
+	/**
+	 * This field will show the status
+	 */
+	private HttpStatus status;
+	/**
+	 * This field represents the reason why the exception occurs
+	 */
+	private String reason;
 	
 	/**
 	 * This field will represent the message which will be set in the constructor of
