39d2458eb72da99c5b10a170197504181b2f9912
Chee Fong <cheefong.ngeow@gmail.com>
2022-06-12 15:37:41 +0800

    added crossorigin annotation

--- dashboard/src/main/java/com/cognizant/dashboard/controller/EmployeeController.java
+++ dashboard/src/main/java/com/cognizant/dashboard/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.cognizant.dashboard.repository.EmployeeRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @Slf4j
 public class EmployeeController {
