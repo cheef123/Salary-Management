6a9ff54788edc328ae87836e79d902ad47ff3c4f
Chee Fong <cheefong.ngeow@gmail.com>
2022-06-16 18:09:13 +0800

    added patch,delete and get mapping

--- dashboard/src/main/java/com/cognizant/dashboard/controller/EmployeeController.java
+++ dashboard/src/main/java/com/cognizant/dashboard/controller/EmployeeController.java
@@ -1,16 +1,23 @@
 package com.cognizant.dashboard.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.dashboard.entity.Employee;
+import com.cognizant.dashboard.exception.ResourceNotFoundException;
 import com.cognizant.dashboard.repository.EmployeeRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -96,6 +103,31 @@ public class EmployeeController {
 
 		return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
 
+	}
+
+	@PatchMapping("/users/{id}")
+	public ResponseEntity<?> updateEmployees(@PathVariable int id, @RequestBody Employee employeeDetails)
+			throws ResourceNotFoundException {
+		Employee employee = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found: " + id));
+		employee.setLogin(employeeDetails.getLogin());
+		employee.setName(employeeDetails.getName());
+		employee.setSalary(employeeDetails.getSalary());
+		return new ResponseEntity<Employee>(repository.save(employee), HttpStatus.OK);
+
+	}
+
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<?> deleteEmployee(@PathVariable int id) throws ResourceNotFoundException {
+		Employee employee = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found: " + id));
+		repository.delete(employee);
+		return new ResponseEntity<String>("Delete successful",HttpStatus.OK);
+	}
+	
+	@GetMapping("/usersAll")
+	public ResponseEntity<?> getAllEmployees(){
+		return new ResponseEntity<List<Employee>>(repository.findAll(),HttpStatus.OK);
 	}
 
 }
